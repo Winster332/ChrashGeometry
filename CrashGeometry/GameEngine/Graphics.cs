be5a578509cc2418e99f8ff31daa8d23975c4836
Stas Martynov <stas-martynov-98@mail.ru>
2016-12-18 00:34:48 +0300

    Описан класс Graphics

--- CrashGeometry/GameEngine/Graphics.cs
+++ CrashGeometry/GameEngine/Graphics.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace CrashGeometry.GameEngine
 {
-	class Graphics
+	public class Graphics
 	{
+		private System.Windows.Forms.PictureBox pBox;
+		private System.Drawing.Image image;
+		private System.Drawing.Graphics graphics;
+
+		public void Initialize(System.Windows.Forms.Form form)
+		{
+			this.pBox = new System.Windows.Forms.PictureBox();
+			this.pBox.Size = form.Size;
+			this.pBox.Location = new System.Drawing.Point(0, 0);
+
+			this.image = new System.Drawing.Bitmap(pBox.Width, pBox.Height);
+			this.graphics = System.Drawing.Graphics.FromImage(image);
+		}
+		public void Begin(System.Drawing.Color color)
+		{
+			graphics.Clear(color);
+		}
+		public void End()
+		{
+			pBox.Image = image;
+		}
+		public void Get()
+		{
+		}
 	}
 }
