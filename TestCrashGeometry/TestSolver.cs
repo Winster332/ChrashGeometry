65841e6fe1300884007f4f2f1323a06883a63fad
Stas Martynov <stas-martynov-98@mail.ru>
2016-12-18 23:42:45 +0300

    Зоздан проект для unit тестирования

---
+++ TestCrashGeometry/TestSolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCrashGeometry
+{
+	[TestClass]
+	public class TestSolver
+	{
+		[TestMethod]
+		public void TestCollisionCircles()
+		{
+
+		}
+		[TestMethod]
+		public void TestCollisionPolygons()
+		{
+
+		}
+		[TestMethod]
+		public void TestCollisionPolygonCircle()
+		{
+
+		}
+	}
+}
