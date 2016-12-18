4b7f5f65bd088b3f443d0103ef971c12f2f0b4e1
Stas Martynov <stas-martynov-98@mail.ru>
2016-12-18 17:59:35 +0300

    Добавлена возможность работы с мышью

--- CrashGeometry/GameEngine/Input.cs
+++ CrashGeometry/GameEngine/Input.cs
@@ -8,5 +8,43 @@ namespace CrashGeometry.GameEngine
 {
 	public class Input
 	{
+		private Game game;
+		public PoolKeyboard PoolKeyboard { get; set; }
+		public PoolMouse PoolMouse { get; set; }
+		public Input(Game game)
+		{
+			this.game = game;
+
+			this.PoolKeyboard = new PoolKeyboard();
+			this.PoolMouse = new PoolMouse();
+
+			this.game.Graphics.GetPicture().MouseDown += Input_MouseDown;
+			this.game.Graphics.GetPicture().MouseUp += Input_MouseUp;
+			this.game.Graphics.GetPicture().MouseMove += Input_MouseMove;
+			this.game.GetForm().KeyDown += Input_KeyDown;
+			this.game.GetForm().KeyUp += Input_KeyUp;
+		}
+		public void Update()
+		{
+			PoolMouse.ClearMouse();
+		}
+		private void Input_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+		{
+		}
+		private void Input_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+		{
+		}
+		private void Input_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			this.PoolMouse.AddMove(e.X, e.Y);
+		}
+		private void Input_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			this.PoolMouse.AddUp(e.X, e.Y);
+		}
+		private void Input_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			this.PoolMouse.AddDown(e.X, e.Y);
+		}
 	}
 }
---
+++ CrashGeometry/GameEngine/PoolMouse.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashGeometry.GameEngine
+{
+	public class PoolMouse
+	{
+		public List<System.Drawing.PointF> Downs { get; set; }
+		public List<System.Drawing.PointF> Ups { get; set; }
+		public List<System.Drawing.PointF> Moves { get; set; }
+		private int maxPool;
+		public PoolMouse()
+		{
+			Downs = new List<System.Drawing.PointF>();
+			Ups = new List<System.Drawing.PointF>();
+			Moves = new List<System.Drawing.PointF>();
+
+			SetMaxPool(5);
+			//AddDown(0, 0);
+			//AddMove(0, 0);
+			//AddUp(0, 0);
+		}
+		public void ClearMouse()
+		{
+			Downs.Clear();
+			Ups.Clear();
+			Moves.Clear();
+		}
+		public void SetMaxPool(int max)
+		{
+			this.maxPool = max;
+		}
+		public int GetMaxPool()
+		{
+			return maxPool;
+		}
+		public void AddDown(float x, float y)
+		{
+			if (Downs.Count > GetMaxPool())
+				Downs.RemoveAt(0);
+			Downs.Add(new System.Drawing.PointF(x, y));
+		}
+		public void AddUp(float x, float y)
+		{
+			if (Ups.Count > GetMaxPool())
+				Ups.RemoveAt(0);
+			Ups.Add(new System.Drawing.PointF(x, y));
+		}
+		public void AddMove(float x, float y)
+		{
+			if (Moves.Count > GetMaxPool())
+				Moves.RemoveAt(0);
+			Moves.Add(new System.Drawing.PointF(x, y));
+		}
+		public System.Drawing.PointF GetLastDown()
+		{
+			return Downs[Downs.Count - 1];
+		}
+		public System.Drawing.PointF GetLastUp()
+		{
+			return Ups[Ups.Count - 1];
+		}
+		public System.Drawing.PointF GetLastMove()
+		{
+			return Moves[Moves.Count - 1];
+		}
+	}
+}
