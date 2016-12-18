de3bccb0d900b4b4209b457684be8c7ea686df6c
Stas Martynov <stas-martynov-98@mail.ru>
2016-12-18 22:54:48 +0300

    Добавлен класс контакта

---
+++ CrashGeometry/Models/Collision/Contact.cs
@@ -0,0 +1,22 @@
+﻿using LMDMono2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashGeometry.Models.Collision
+{
+	public class Contact
+	{
+		public Dot Position { get; set; }
+		public Model Model1 { get; set; }
+		public Model Model2 { get; set; }
+		public Contact(Dot position, Model m1, Model m2)
+		{
+			this.Position = position;
+			this.Model1 = m1;
+			this.Model2 = m2;
+		}
+	}
+}
