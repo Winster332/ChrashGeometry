6a251b8af99d6ed6082c028d827509eec6acfd96
Stas Martynov <stas-martynov-98@mail.ru>
2016-12-18 19:07:01 +0300

    Проект пересобран на фреймверк 4.5.2

--- CrashGeometry/MainForm.cs
+++ CrashGeometry/MainForm.cs
@@ -18,27 +18,20 @@ namespace CrashGeometry
 		public MainForm()
 		{
 			InitializeComponent();
-
-
-
+			
 			timer = new Timer();
 			timer.Interval = 30;
 			timer.Tick += Timer_Tick;
 		}
-
-		private void Timer_Tick(object sender, EventArgs e)
-		{
-			if (game.Input.PoolKeyboard.Downs.Count > 0)
-				Console.WriteLine("" + game.Input.PoolKeyboard.GetDown(Keys.A));
-			game.Update();
-		}
-
 		private void MainForm_Load(object sender, EventArgs e)
 		{
 			game = new Game(this);
+			game.SetScreen(new Screens.MainScreen());
 			timer.Start();
-
-
+		}
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			game.Update();
 		}
 	}
 }
