22e3a845540aecf266b88dad525343bacffe5ff5
Stas Martynov <stas-martynov-98@mail.ru>
2016-12-24 01:03:24 +0300

    Создана база для систем частиц

---
+++ CrashGeometry/SystemParticle/BaseParticle.cs
@@ -0,0 +1,23 @@
+﻿using CrashGeometry.Models;
+using LMDMono2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashGeometry.SystemParticle
+{
+	public abstract class BaseParticle : Model
+	{
+		public bool IsDead { get; internal set; }
+		public Dot Velocity { get; set; }
+
+		protected BaseParticle()
+		{
+			Velocity = new Dot(0, 0);
+		}
+
+		public abstract void Update();
+	}
+}
---
+++ CrashGeometry/SystemParticle/BaseSystemParticle.cs
@@ -0,0 +1,35 @@
+﻿using CrashGeometry.GameEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashGeometry.SystemParticle
+{
+	public abstract class BaseSystemParticle
+	{
+		public List<BaseParticle> Particles { get; set; }
+		protected Game game { get; set; }
+		protected BaseSystemParticle(Game game)
+		{
+			this.game = game;
+		}
+		public abstract void Add(float x, float y, int count = 1);
+		public void Draw()
+		{
+			for (int i = 0; i < Particles.Count; i++)
+			{
+				var particle = Particles[i];
+				if (particle.IsDead)
+				{
+					Particles.RemoveAt(i);
+					continue;
+				}
+
+				particle.Update();
+				particle.Draw();
+			}
+		}
+	}
+}
